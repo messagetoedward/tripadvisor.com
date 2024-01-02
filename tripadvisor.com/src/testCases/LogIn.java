7ae11de6950c8da37bef6598675ad25a899f5150
Edward Zaplotynskyi <edward.downtown@gmail.com>
2024-01-01 23:03:20 -0500

    initial push

---
+++ tripadvisor.com/src/testCases/LogIn.java
@@ -0,0 +1,29 @@
+package testCases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class LogIn {
+	
+	@Test
+	public void LogInStart() {
+		
+		System.setProperty("webdriver.chrome.driver", "/Users/mgid/Downloads/chromedriver-mac-x64/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://tripadvisor.com");
+		
+	//	System.out.println(driver.getTitle());
+		//	System.out.println(driver.getTitle());
+
+		driver.quit();
+		
+		
+		
+	}
+
+	
+	
+}
